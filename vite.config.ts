20add9cbaaae3fb091c8aa84e4a0c619a842b70e
Pablo <pablorottendev@gmail.com>
2026-03-19 03:07:18 +0100

    issues with npm run deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/vite-react-tailwind/' : '/'
+  base: import.meta.env.PROD ? '/vite-react-tailwind/' : '/'
 })
