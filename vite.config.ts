d873361970d0e4ffd4a2546e852cd34d97f3bcdf
Pablo <pablorottendev@gmail.com>
2026-03-19 03:08:21 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.PROD ? '/vite-react-tailwind/' : '/'
-})
+  base: mode === 'production' ? '/vite-react-tailwind/' : '/',
+}));
